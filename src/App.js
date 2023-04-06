678a211dec5e00dc4d1bf0185de16734e9f60eb8
Nana Chuto <mail@nanachuto.com>
2023-04-06 16:51:32 +0200

    Added some line breaks

--- src/App.js
+++ src/App.js
@@ -38,7 +38,9 @@ function App() {
   return (
     <div className="App">
       <Tweet tweet={tweetsArray[0]} />
+      <br />
       <Tweet tweet={tweetsArray[1]} />
+      <br />
       <Tweet tweet={tweetsArray[2]} />
     </div>
   );

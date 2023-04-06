70014d4c0e64ce4c07dfd6a9aa3bdf72ec8cc3ef
Nana Chuto <mail@nanachuto.com>
2023-04-06 16:19:13 +0200

    Iteration 4 done

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -5,30 +5,7 @@ function Tweet(props) {
 
   return (
     <div className="tweet">
-      <img src={user.image} className="profile" alt="profile" />
-
-      <div className="body">
-        <div className="top">
-          <span className="user">
-            <span className="name">{user.name}</span>
-            <span className="handle">@{user.handle}</span>
-          </span>
-
-          <span className="timestamp">{timestamp}</span>
-        </div>
-
-        <p className="message">{message}</p>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment"></i>
-          <i className="fas fa-retweet"></i>
-          <i className="far fa-heart"></i>
-          <i className="fas fa-share"></i>
-        </div>
-      </div>
-
-      <i className="fas fa-ellipsis-h"></i>
+      <ProfileImage image={props.tweet.user.image} />
     </div>
   );
 }

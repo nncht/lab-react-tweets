18b1053aac4ff2552dae910b6906d282639b6609
Nana Chuto <mail@nanachuto.com>
2023-04-06 16:46:29 +0200

    Iteration 8 done

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,6 +1,7 @@
 import ProfileImage from "./ProfileImage";
 import User from "./User";
 import Timestamp from "./Timestamp";
+import Message from "./Message";
 
 function Tweet(props) {
   return (
@@ -12,6 +13,7 @@ function Tweet(props) {
           <User userData={props.tweet.user} />
           <Timestamp time={props.tweet.timestamp} />
         </div>
+        <Message message={props.tweet.message} />
       </div>
     </div>
   );
